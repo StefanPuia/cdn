b7c19490a6575d428bcef753ced74e01c6ee6a0b
Stefan <24592655+StefanPuia@users.noreply.github.com>
2020-01-04 23:52:47 +0000

    Update mal-inject.js

--- mal-inject.js
+++ mal-inject.js
@@ -1,10 +1,8 @@
-$(function () {
-    $("img.hover-info.image").each((k, img) => {
-        const regex = new RegExp("(https://cdn.myanimelist.net/).+?(images/anime/.+?).webp.+");
-        const old = img.src;
-        const match = old.match(regex);
-        if (match) {
-            img.src = old.replace(regex, "$1$2.jpg");
-        }
-    });
-});
+for(const img of document.querySelectorAll("img.hover-info.image")) {
+    const regex = new RegExp("(https://cdn.myanimelist.net/).+?(images/anime/.+?).webp.+");
+    const old = img.src;
+    const match = old.match(regex);
+    if (match) {
+        img.src = old.replace(regex, "$1$2.jpg");
+    }
+}
