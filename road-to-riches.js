59f2810373b24cfd05c94ed16a93ef280c71ce69
Stefan <24592655+StefanPuia@users.noreply.github.com>
2020-05-12 12:30:06 +0100

    Create road-to-riches.js

---
+++ road-to-riches.js
@@ -0,0 +1,20 @@
+function __applyListeners() {
+    const $tableToggles = $("div.col-md-11 > div > div > div.card-body > div > table > tbody > tr > td:nth-child(1)");
+    $tableToggles.off("click", __toggleBG);
+    $tableToggles.on("click", __toggleBG);
+
+    function __toggleBG(e) {
+        const $td = $(e.currentTarget);
+        const $tr = $td.parent();
+        if ($tr.data("__toggle") == true) {
+            $tr.find("td").css("background-color", "unset");
+            $tr.find("td").css("color", "unset");
+        } else {
+            $tr.find("td").css("background-color", "black");
+            $tr.find("td").css("color", "white");
+        }
+        $tr.data("__toggle", !$tr.data("__toggle"))
+    }
+}
+
+setInterval(__applyListeners, 5000);
